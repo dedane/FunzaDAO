c977e9a6a8d75f37f2f73c81a4190532fac26046
dedane <evanskeema@gmail.com>
2022-03-28 23:14:17 +0300

    added-thirdweb-connect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { useAddress, useMetamask } from '@thirdweb-dev/react';
+
 const App = () => {
+
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  console.log(" address:", address);
+
+  if(!address) {
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1> 👨‍💻   Welcome to FunzaDAO 👩‍💻 </h1>
+      <button onClick={connectWithMetamask}>Connect your wallet</button>
     </div>
   );
-};
+}
 
+return (
+  <div className='landing'>
+    <h1>Wallet connected, now what?</h1>
+    </div>
+)
+}
 export default App;
