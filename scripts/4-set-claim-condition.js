86ff498051e2393462db3e4928b977a151f9b155
dedane <evanskeema@gmail.com>
2022-03-31 21:07:40 +0300

    adding-edition-drop-checks

---
+++ scripts/4-set-claim-condition.js
@@ -0,0 +1,21 @@
+import sdk from './1-initialize-sdk.js'
+import { MaxUint256 } from '@ethersproject/constants'
+
+const editionDrop = sdk.getEditionDrop(process.env.EditionDropAddress);
+
+(async() => {
+ try{
+    const claimConditions = [{
+        staetTime: new Date(),
+        maxQuantity: 50000,
+        price: 0,
+        quanityLimitPerTransaction: 1,
+        waitInSeconds: MaxUint256,
+    }]
+    await editionDrop.claimConditions.set("0", claimConditions);
+    console.log("Successfully set claim condition")
+ }
+ catch(err){
+    console.errpr("Failed to claim Nft", err)
+ }
+})();
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
-import { useAddress, useMetamask } from '@thirdweb-dev/react';
+import { useAddress, useMetamask, useEditionDrop } from '@thirdweb-dev/react';
+import { EditionDrop } from '@thirdweb-dev/sdk';
+import {useState, useEffect} from 'react'
+import dotenv from "dotenv";
+dotenv.config();
 
 const App = () => {
 
   const address = useAddress();
   const connectWithMetamask = useMetamask();
   console.log(" address:", address);
+
+  //Edition drop address on rinkeby testnet
+  const editionDrop  = useEditionDrop(process.env.EditionDropAddress);
+  //State variable to check if they have an Nft
+  const [hasClaimedNft, setHasClaimedNft] = useState(false);
+
+  useEffect(() => {
+    //If they dont have a connected wallet
+    if(!address){
+      return
+    }
+    const checkBalance = async () => {
+      try {
+        const balance = await EditionDrop.balanceOf(address, 0);
+        if(balance.gt(0)){
+          setHasClaimedNft(true);
+          console.log("This used has a membership NFT!")
+        } else {
+          setHasClaimedNft(false);
+          console.log(" This user does not have a membership Nft")
+        }
+      }
+      catch(err){
+        setHasClaimedNft(false);
+        console.error("Failed to claim and Nft", err)
+
+      }
+    }
+    checkBalance()
+  },[address, editionDrop])
 
   if(!address) {
   return (
