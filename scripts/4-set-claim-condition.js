1e3cac17874b63879dec5928fd87f2b8d74888b9
dedane <evanskeema@gmail.com>
2022-04-06 05:39:51 +0300

    mint-nft-for-each-new-dao-member

--- scripts/4-set-claim-condition.js
+++ scripts/4-set-claim-condition.js
@@ -1,6 +1,7 @@
 import sdk from './1-initialize-sdk.js'
 import { MaxUint256 } from '@ethersproject/constants'
 
+
 const editionDrop = sdk.getEditionDrop(process.env.EditionDropAddress);
 
 (async() => {
--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,10 @@ const App = () => {
   console.log(" address:", address);
 
   //Edition drop address on rinkeby testnet
-  const editionDrop  = useEditionDrop (process.env.EditionDropAddress);
+const editionDrop  = useEditionDrop("0x35F3FAC08975eA1dE1Ae8ce4FFbF743592a4c320");
+  
   //State variable to check if they have an Nft
-  const [hasClaimedNft, setHasClaimedNft] = useState(false);
+  const [hasClaimedNFT, setHasClaimedNft] = useState(false);
   //State variable to keep a loading screen while  NFT is minting
   const [isClaiming, setIsClaiming] = useState(false)
 
@@ -24,10 +25,10 @@ const App = () => {
     }
     const checkBalance = async () => {
       try {
-        const balance = await EditionDrop.balanceOf(address, 0);
+        const balance = await editionDrop.balanceOf(address, 0);
         if(balance.gt(0)){
           setHasClaimedNft(true);
-          console.log( "🚀 This used has a membership NFT!")
+          console.log( "🚀 This user has a membership NFT!")
         } else {
           setHasClaimedNft(false);
           console.log( " ☠ This user does not have a membership Nft")
@@ -45,7 +46,7 @@ const App = () => {
     try {
       setIsClaiming(true);
       await editionDrop.claim("0", 1);
-      console.log( ` 🌊 Successfully Minted! check it out on OpenSea: http://testnets.opensea.io/assets/${editionDrop.address}/0`);
+      console.log(`🌊 Successfully Minted! Check it out on OpenSea: https://testnets.opensea.io/assets/${editionDrop.getAddress()}/0`);
       setHasClaimedNft(true)
     } catch(error){
       setHasClaimedNft(false);
@@ -59,19 +60,28 @@ const App = () => {
   if(!address) {
   return (
     <div className="landing">
-      <h1> 👨‍💻   Welcome to FunzaDAO 👩‍💻 </h1>
-      <button onClick={connectWithMetamask}>Connect your wallet</button>
+      <h1> 👨‍💻 Welcome to FunzaDAO 👩‍💻 </h1>
+      <button onClick={connectWithMetamask} className="btn-hero">
+        Connect your wallet
+        </button>
     </div>
   );
 }
-
+if(hasClaimedNFT){
+  return(
+    <div className="member-page">
+      <h1>DAO MEMBER PAGE</h1>
+      <p>Congratulations on being a member</p>
+    </div>
+  )
+}
 return (
   <div className='mint-nft'>
     <h1>Mint your free 👨‍🏫👩‍🏫  DAO membership NFT</h1>
     <button
       disabled={isClaiming}
       onClick={mintNft}>
-        {isClaiming ? "minting..." : "Mint your free Nft (FREE)"}
+        {isClaiming ? "Minting..." : "Mint your free Nft (FREE)"}
       </button>
     </div>
 )
